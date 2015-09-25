da4de50984c61176f6d5df5b894ff5161dc40c4b
Joe Barnett <joe@px8soutions.com>
2015-09-24 21:25:23 -0400

    more OOP

---
+++ src/Beboputer/RAM.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Beboputer
+{
+    public class RAM
+    {
+        private byte[] _ram = new byte[65536];
+
+
+        public byte this[int address]
+        {
+            get
+            {
+                return Read(address);
+            }
+            set
+            {
+                Write(address,value);
+            }
+        }
+
+        public byte Read(int address)
+        {
+            return _ram[address];
+        }
+
+        public byte Read(byte low, byte high)
+        {
+            return _ram[To16(low,high)];
+        }
+
+        public byte ReadIndirect(int low, int high)
+        {
+            byte h = this[To16(low,high) + 1];
+            byte l = this[To16(low,high) + 2];
+
+            byte h2 = Read(l, h);
+            byte l2 = this[RAM.To16(l, h) + 1];
+
+
+
+            return _ram[To16(l2, h2)];
+        }
+
+
+
+        public void Write(int address, byte data)
+        {
+            _ram[address]=data;
+        }
+
+        public void Write(byte low, byte high, byte data)
+        {
+            _ram[To16(low,high)] = data;
+        }
+
+        public static int To16(int low, int high)
+        {
+            return (high << 8) + low;
+        }
+
+    }
+}
---
+++ src/Beboputer/RegisterFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Beboputer
+{
+    public class RegisterFile
+    {
+        private byte _accumulator = 0;
+        private byte _programCounter = 0;
+        private byte _instructionRegister = 0;
+
+        public void Reset()
+        {
+            _accumulator = 0;
+            _programCounter = 0;
+            _instructionRegister = 0;
+        }
+
+        public byte IR
+        {
+            set { _instructionRegister=value; }
+            get { return _instructionRegister; }
+        }
+
+        public byte PC
+        {
+            set { _programCounter = value; }
+            get { return _programCounter; }
+        }
+
+        public byte A
+        {
+            set { _accumulator = value; }
+            get { return _accumulator; }
+        }
+
+    }
+}
